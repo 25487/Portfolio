ddbc6f69a0c0f912239a50fa263f65ba4a98a06a
Taurese <43673192+25487@users.noreply.github.com>
2019-11-10 16:48:49 +0100

    Delete index.js

--- portfolio/index.js
+++
@@ -1,7 +0,0 @@
-function test(){
- url = "https://www.google.de//images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
- img = '<img src="'+url+'">';
- popup = window.open();
- popup.document.write(img);
- popup.print();
- }
